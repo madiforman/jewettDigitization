6b943636a84670f139c6e9882a00879046f7c22c
Seung Park <seung@Seungs-MacBook-Air.local>
2023-03-03 13:18:10 -0800

    took Madi's code and deleted unused code/comments

--- scripts/image_processing.py
+++ scripts/image_processing.py
@@ -6,16 +6,10 @@
 import numpy as np
 import cv2 as cv
 import glob
-import os,sys 
-from matplotlib import pyplot as plt
-from pathlib import Path
  
 path0 = '/Users/seung/Documents/Spring23/jewettDigitization/data/*.jpg' 
-#jewett scans is not currently in here, should be 44 images
-# path1 = '/Users/madisonforman/Desktop/processing/data/jewettscans/*.jpg'
 cur = 0
 def create_names(path):
-    # images = [f for f in os.listdir(dir)]
     lang = 'eng'
     font = 'jewett'
     str = f"{lang}.{font}.exp"
@@ -25,7 +19,6 @@ def create_names(path):
         filename = f"{str}{i}.jpg"
         names.append(filename)
         i += 1
-        # os.rename(os.path.join(dir, image), os.path.join(dir,filename))
     return names
 
 def load_images(path):
@@ -34,86 +27,87 @@ def load_images(path):
         i = cv.imread(img)
         cv_imgs.append(i)
     return cv_imgs
-#alpha controls brightness, beta controls contrast
+
+def remove_horizontal_lines(image):
+
+    thresh = cv.threshold(image, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
+    horizontal_kernel = cv.getStructuringElement(cv.MORPH_RECT, (35, 1))
+    image1 = cv.dilate(thresh, horizontal_kernel, iterations=1) 
+
+    detected_lines = cv.morphologyEx(image1, cv.MORPH_OPEN, horizontal_kernel, iterations = 2)
+    cv.imshow('lines', detected_lines)
+
+    contours = cv.findContours(detected_lines, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+
+    for c in contours:
+        cv.drawContours(image, [c], -1, (255, 255, 255), 2)
+
+    return image
+
+def remove_horizontal(image):
+    h = float(image.shape[0])
+    maxVal = 250
+    blockSize = 15
+    C = 12.0*(90.0/h)
+    bw = cv.adaptiveThreshold(image, maxVal, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, blockSize, C)
+    bw = ~bw
+    vertical = bw.copy()
+    verticalStructure = cv.getStructuringElement(cv.MORPH_RECT, (1, 5))
+    vertical = cv.erode(vertical, verticalStructure, None, (-1,-1))
+    vertical = cv.dilate(vertical, verticalStructure, None, (-1,-1))
+    vertical = ~vertical
+    cv.imshow("remove_horiz", vertical)
+    return vertical
+
+def remove_noise(image):
+    blur = cv.GaussianBlur(image,(13,13),0)
+    thresh = cv.threshold(blur, 100,255, cv.THRESH_BINARY)[1]
+    cv.imshow("final 2", thresh)
+
+def repair_image(image):
+    ret, thresh1 = cv.threshold(image, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+    kernel = np.array((5, 2), np.uint8)
+    closing = cv.morphologyEx(thresh1, cv.MORPH_CLOSE, kernel)
+    remove_noise(image)
+
+def remove_lines(image):
+    image = remove_horizontal(image)
+    edges = cv.Canny(image, 50, 150, apertureSize=3)
+    lines = cv.HoughLinesP(edges, 1, np.pi/180, 150, minLineLength=50, maxLineGap = 150)
+    for line in lines:
+        x0, y0, x1, y1 = line[0]
+        cv.line(image, (x0, y0), (x1, y1), (255, 0, 0), 3)
+    cv.imshow("remove lines", image)
+
+    repair_image(image)
+
+"""
+Preprocess images does the image preprocessing
+- alpha controls brightness, beta controls contrast
+- alpha range: 0 < alpha < 1
+- beta range:  [-127, 127]
+""" 
 def preprocess_images(path, alpha, beta):
     cur = 0
     image_list = load_images(path)
     name_list = create_names(path)
-
-    print(name_list)
     i = 0
     for image in image_list:
         gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-        h = float(gray.shape[0])
-        C = 12.0*(90.0/h)
-        #1 grayscale and contrast
-        
-        
-        bw = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 15, C)
-        bw = ~bw
-        vertical = bw.copy()
-        verticalsize = 5
-        verticalStructure = cv.getStructuringElement(cv.MORPH_RECT, (1, verticalsize))
-        vertical = cv.erode(vertical, verticalStructure, None, (-1,-1))
-        vertical = cv.dilate(vertical, verticalStructure, None, (-1,-1))
-        vertical = ~vertical
-        # cv.imshow("vertical", vertical)
-        
-        thresh = cv.threshold(vertical, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
-        horizontal_kernel = cv.getStructuringElement(cv.MORPH_RECT, (45, 1))
-        image1 = cv.dilate(thresh, horizontal_kernel, iterations=1) 
-        detected_lines = cv.morphologyEx(image1, cv.MORPH_OPEN, horizontal_kernel, iterations = 3)
-        contours = cv.findContours(detected_lines, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-        contours = contours[0] if len(contours) == 2 else contours[1]
-        for c in contours:
-            cv.drawContours(vertical, [c], -1, (255, 255, 255), 2)
-        cv.imshow('gray', vertical)
-        
-        # gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-        # adjusted = cv.convertScaleAbs(gray, alpha=alpha, beta=beta)
-        #2 blur and divide m 
-        # thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
-        # horizontal_kernel = cv.getStructuringElement(cv.MORPH_RECT, (45, 1))
-        # image1 = cv.dilate(thresh, horizontal_kernel, iterations=1) 
-        # detected_lines = cv.morphologyEx(image1, cv.MORPH_OPEN, horizontal_kernel, iterations = 3)
-        # contours = cv.findContours(detected_lines, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-        # contours = contours[0] if len(contours) == 2 else contours[1]
-        # # cv.imshow('lines', detected_lines)
-        # for c in contours:
-        #     cv.drawContours(gray, [c], -1, (255, 255, 255), 2)
-        # # cv.imshow('gray', gray)
-        # horizontal_kernel = cv.getStructuringElement(cv.MORPH_RECT, (3, 1))
-        # vertical = cv.erode(gray, horizontal_kernel, None, (-1,-1))
-        # dilated = cv.dilate(vertical, horizontal_kernel, None, (-1,-1))
-        # cv.imshow('dilated', dilated)
-        
-        #dividing all the values by 255 will convert it to range from 0 to 1.
-        # blur = cv.GaussianBlur(adjusted, (0,0), sigmaX=33, sigmaY=33)
-        # divide = cv.divide(adjusted, blur, scale=225)
-        # gray_filtered = cv.inRange(divide, 0, 100)
-        #3 binarization
-        # _, thresh = cv.threshold(adjusted, 200, 255, cv.THRESH_BINARY)
-        # normalized = cv.normalize(adjusted, None, 0, 1.0, cv.NORM_MINMAX, dtype=cv.CV_32F)
-        #4 noise removal
-        # kernel = cv.getStructuringElement(cv.MORPH_RECT, (1,1))
-        # morph = cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel)
-        # erosion = cv.erode(morph, kernel, iterations=1)
-        #cv.imshow("thresh", thresh)
-        # cv.imshow("morph", morph)
-        # cv.imshow("og", image)
-        # cv.imshow("gray filter", gray_filtered)
-        # cv.imshow("normalized", normalized)
+        gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        adjusted = cv.convertScaleAbs(gray, alpha=alpha, beta=beta)
+        remove_lines(gray)
+        other = remove_horizontal(gray)
+
         cv.waitKey()
         cv.destroyAllWindows()
         cur += 1
-        if cur == 5:
+        if cur == 10:
             break
-        # os.chdir('/Users/madisonforman/Desktop/processing/data')
-        # cv.imwrite(name_list[i], morph)
         i += 1
-preprocess_images(path0, 1, -10)
-#preprocess_images(path1, 1, 30)
 
+preprocess_images(path0, 1, -10)
 
 
 """
@@ -135,49 +129,3 @@ find a premade ocr that gets higher accuracy, and correct that instead
 have one/two person scanning and transcribing
 other two people find a dataset as close as possible and begin training that
 """
-# FOLLOWING THIS LINE FUNCTIONS HAVE NOT BEEN WORKING SUPER WELL <3 MIGHT BE BETTER ON SEGMENTED IMAGES
-#https://becominghuman.ai/how-to-automatically-deskew-straighten-a-text-image-using-opencv-a0c30aed83df
-def getSkewAngle(cvImage):
-    new = cvImage.copy()
-    gray = cv.cvtColor(new, cv.COLOR_BGR2GRAY)
-    blur = cv.GaussianBlur(gray, (9, 9), 0)
-    thresh = cv.threshold(blur, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
-
-    kernel = cv.getStructuringElement(cv.MORPH_RECT, (30, 5))
-    dilate = cv.dilate(thresh, kernel, iterations=5)
-
-    contours, hierarchy = cv.findContours(dilate, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key = cv.contourArea, reverse = True)
-    
-    largest_countour = contours[0]
-    min_area_rect = cv.minAreaRect(largest_countour)
-    angle = min_area_rect[-1]
-    if angle < -45:
-        angle += 90
-    return -1.0 * angle
-def rotate_image(cvImage, angle):
-    new = cvImage.copy()
-    (h,w) = new.shape[:2]
-    center = (w // 2, h // 2)
-    M = cv.getRotationMatrix2D(center, angle, 1.0)
-    new = cv.warpAffine(new, M, (w,h),
-    flags = cv.INTER_CUBIC, borderMode = cv.BORDER_REPLICATE)
-    return new
-
-def deskew(cvImage):
-    angle = getSkewAngle(cvImage)
-    return rotate_image(cvImage, -1.0 * angle)
-
-def remove_borders(cvImage):
-    new = cvImage.copy()
-    mask = np.zeros(new.shape, dtype=new.dtype)
-    _, thresh = cv.threshold(new, 205, 255, cv.THRESH_BINARY)
-    cont = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    cont = cont[0] if len(cont) == 2 else cont[1]
-
-    for c in cont:
-        area = cv.contourArea(c)
-        if area < 500:
-            cv.drawContours(mask, [c], -1, (255, 255, 255), -1)
-    removed_boarder = cv.bitwise_and(new, new, mask=mask)
-    return removed_boarder
